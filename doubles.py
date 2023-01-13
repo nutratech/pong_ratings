c05e1f21fb871870820f9290be6398ffa6367774
Shane Jaroch <chown_tee@proton.me>
2023-01-13 13:54:09 -0500

    print singles/doubles at top

--- doubles.py
+++ doubles.py
@@ -212,6 +212,7 @@ def print_matchups(players: List[Player]):
 
 if __name__ == "__main__":
     # NOTE: Also need to support DOUBLES rankings & matches (not just singles)
+    print("DOUBLES")
     print(f"Last updated: {date.today()}")
     _sorted_players = build_ratings()
     print_matchups(_sorted_players)
