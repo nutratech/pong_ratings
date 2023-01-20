96a5dcc6e4a20f54f30a8aa762a14183480b0727
Shane Jaroch <chown_tee@proton.me>
2023-01-20 15:51:03 -0500

    add game odds

--- pong/probs.py
+++ pong/probs.py
@@ -25,6 +25,33 @@ def p_deuce(p: float) -> Dict[int, float]:
     }
 
 
+def p_deuce_win(p: float) -> float:
+    """
+    Get probability of winning at deuce (two points in a row).
+    :param p: Probability of winning an individual point
+    """
+    return p**2 / (p**2 + (1 - p) ** 2)
+
+
+def p_game(p: float) -> Dict[int, float]:
+    """
+    Probability of winning a game, based on probability to win a point.
+    :param p: Probability of winning an individual point
+    """
+
+    def _p_n(n: int) -> float:
+        _sum = 0.0
+        for k in range(0, n - 1):
+            _sum += p**n * (1 - p) ** k * (math.comb(n - 1 + k, k))
+        _sum += p_deuce(p)[n] * p_deuce_win(p)
+        return round(_sum, 3)
+
+    return {
+        11: _p_n(11),
+        21: _p_n(21),
+    }
+
+
 def match_odds(p: float) -> Dict[int, float]:
     """
     Calculate probability to win a match (best of 3 & best of 5), based on probability
@@ -45,8 +72,25 @@ def print_table_common_deuce_odds() -> None:
     _series = []
     for _po in [0.5, 0.51, 0.55, 0.6, 0.7, 0.8]:
         _do = p_deuce(_po)
-        _11go = _do[11]
-        _21go = _do[21]
+        _11go = round(_do[11], 3)
+        _21go = round(_do[21], 3)
+        _series.append((_po, _11go, _21go))
+
+    _table = tabulate(
+        _series,
+        headers=["P(p)", "P(11g)", "P(21g)"],
+    )
+    print(_table)
+
+
+def print_table_common_game_odds() -> None:
+    """Print a table for common game odds"""
+    print("Game odds")
+    _series = []
+    for _po in [0.5, 0.51, 0.55, 0.6, 0.7, 0.8]:
+        _go = p_game(_po)
+        _11go = _go[11]
+        _21go = _go[21]
         _series.append((_po, _11go, _21go))
 
     _table = tabulate(
@@ -75,5 +119,5 @@ def print_table_common_match_odds() -> None:
 
 if __name__ == "__main__":
     print_table_common_match_odds()
+    print_table_common_game_odds()
     print_table_common_deuce_odds()
-
