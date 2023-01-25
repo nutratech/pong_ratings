9498d194a6c698fb89e096bc5bc55a79ea3a1c87
Shane Jaroch <chown_tee@proton.me>
2023-01-25 17:24:37 -0500

    rename, why always underscore names?

--- pong/__init__.py
+++ pong/__init__.py
@@ -12,10 +12,10 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Hard-coded URL values pointing to our sheet
 def _url(gid: int) -> str:
-    _spreadsheet_key = "1evcgUzJ5hO55RYshc3dH-EmzZfor58t0qPB-zp8iw4A"
+    spreadsheet_key = "1evcgUzJ5hO55RYshc3dH-EmzZfor58t0qPB-zp8iw4A"
     return (
         "https://docs.google.com/spreadsheet/ccc"
-        f"?key={_spreadsheet_key}"
+        f"?key={spreadsheet_key}"
         f"&gid={gid}"
         "&output=csv"
     )
