5bc8551321ddbb823b55582fdc1ef477733b1158
Shane Jaroch <chown_tee@proton.me>
2023-01-26 16:33:04 -0500

    add todo for env spreadsheet key/gid

--- pong/env.py
+++ pong/env.py
@@ -11,6 +11,8 @@ import dotenv
 
 dotenv.load_dotenv(verbose=True)
 
+# TODO: store spreadsheet key and gid in .env file?
+
 # TODO: support inverse too? Inactive players
 PLAYERS_PRESENT = os.environ.get("PLAYERS")
 if PLAYERS_PRESENT:
