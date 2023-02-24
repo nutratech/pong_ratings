772371a195dbe00f79e8cf75bc3fcda20a41d7a6
Shane Jaroch <chown_tee@proton.me>
2023-02-24 12:08:56 -0500

    fix recursive error with dotenv dep in __init__

--- pong/env.py
+++ pong/env.py
@@ -9,6 +9,8 @@ import os
 
 import dotenv
 
+from pong import DOUBLES, SINGLES
+
 dotenv.load_dotenv(verbose=True)
 
 # TODO: store spreadsheet key and gid in .env file?
@@ -23,3 +25,23 @@ MODE_SINGLES = not int(os.environ.get("PONG_DOUBLES") or 0)
 PONG_SHEET_KEY = os.environ["PONG_SHEET_KEY"]
 PONG_SHEET_GID_SINGLES = int(os.environ["PONG_SHEET_GID_SINGLES"])
 PONG_SHEET_GID_DOUBLES = int(os.environ["PONG_SHEET_GID_DOUBLES"])
+
+
+# URLs
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def _url(gid: int) -> str:
+    """Hard-coded URL values pointing to our sheet"""
+
+    return (
+        "https://docs.google.com/spreadsheet/ccc"
+        f"?key={PONG_SHEET_KEY}"
+        f"&gid={gid}"
+        "&output=csv"
+    )
+
+
+# URLs to Google Sheets for singles & doubles CSVs
+CSV_GAMES_URLS = {
+    SINGLES: _url(PONG_SHEET_GID_SINGLES),
+    DOUBLES: _url(PONG_SHEET_GID_DOUBLES),
+}
